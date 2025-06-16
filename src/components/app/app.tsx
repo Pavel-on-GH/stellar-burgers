2c660733499fa1bccb9dc0f96e50e691ff61517f
Pavel-on-GH <Pavel.Zaitsev-7@yandex.ru>
2025-06-16 23:51:08 +0300

    1.0.1.0 - подключение react-router-dom; добавлены роуты страниц;

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,60 @@
-import { ConstructorPage } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { Route, Routes } from 'react-router-dom';
 
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  NotFound404,
+  ProfileOrders,
+  Profile
+} from '@pages';
 import { AppHeader } from '@components';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />}>
+        ConstructorPage
+      </Route>
+
+      <Route path='/feed' element={<Feed />}>
+        Feed
+      </Route>
+
+      <Route path='/login' element={<Login />}>
+        Login
+      </Route>
+
+      <Route path='/register' element={<Register />}>
+        Register
+      </Route>
+
+      <Route path='/forgot-password' element={<ForgotPassword />}>
+        ForgotPassword
+      </Route>
+
+      <Route path='/reset-password' element={<ResetPassword />}>
+        ResetPassword
+      </Route>
+
+      <Route path='/profile' element={<Profile />}>
+        Profile
+      </Route>
+
+      <Route path='/profile/orders' element={<ProfileOrders />}>
+        ProfileOrders
+      </Route>
+
+      <Route path='/*' element={<NotFound404 />}>
+        NotFound404
+      </Route>
+    </Routes>
   </div>
 );
 
